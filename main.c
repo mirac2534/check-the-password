777accef96f1c455bc557a428dc037bf0aee2655
Miraç Furkan Durmaz <117925817+mirac2534@users.noreply.github.com>
2024-11-02 18:16:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_TRIES 5
-#define PASSWORD 1234
-int checkPasswordWithFor();
-int checkPasswordWithWhile();
-int checkPasswordWithDoWhile();
+#define MAX_TRIES 5 // The right to maximum trial
+#define PASSWORD 1234 // This is your password
+int checkPasswordWithFor(); // Learn to use 'for' loop
+int checkPasswordWithWhile(); // Learn to use 'while' loop
+int checkPasswordWithDoWhile(); // Learn to use 'do-while' loop
 
 int main()
 {
 int success;
-//success=checkPasswordWithFor();
-//success=checkPasswordWithWhile();
+success=checkPasswordWithFor();
+success=checkPasswordWithWhile();
 success=checkPasswordWithDoWhile();
  if (success)
 {
@@ -22,23 +22,23 @@ else
 }
     return 0;
 }
-int checkPasswordWithWhile()
+
+int checkPasswordWithFor()
 {
     int i,userPassword;
-    for(i=0;i<MAX_TRIES;i++)
+    for(i=0;i<MAX_TRIES;i++) // for(yourVariable, condition, process)
     {
         printf("enter your password");
         scanf("%d",&userPassword);
         if (userPassword==PASSWORD)
             return 1;
     }
-
-
-
+// In cases where it is unclear how many times it will return, and the condition is controlled according to an input, the While or do-while cycle is used, while in cases where the number of repetitions is certain 
+// such as arrays, it is preferable to use the for cycle.
      int checkPasswordWithWhile ()
      {
          int i=0, userPassword;
-         while(i<MAX_TRIES)
+         while(i<MAX_TRIES) // while(condition)
          {
              printf("enter your password");
              scanf("%d",&userPassword);
@@ -50,6 +50,7 @@ int checkPasswordWithWhile()
      }
         return 0;
 }
+// The do-while loop performs the operation first and then checks the condition
 int checkPasswordWithDoWhile()
 {
     int i=0,userPassword;
